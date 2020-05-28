d87c3d2e14c0bd7c9c0e7d34a2a82f9c49d5df89
Srdjan Stankov <stankovsr@gmail.com>
2020-05-28 22:22:48 +0200

    Removed unused variable

--- Predmetni_zadatak_3_Grafika/MainWindow.xaml.cs
+++ Predmetni_zadatak_3_Grafika/MainWindow.xaml.cs
@@ -26,7 +26,6 @@ namespace Predmetni_zadatak_3_Grafika
         private bool rotate = false;
         private double xScale;
         private double yScale;
-        private GeometryModel3D hitgeo;
         private List<SubstationEntity> substationEntities = new List<SubstationEntity>();
         private List<NodeEntity> nodeEntities = new List<NodeEntity>();
         private List<SwitchEntity> switchEntities = new List<SwitchEntity>();
@@ -266,7 +265,6 @@ namespace Predmetni_zadatak_3_Grafika
                 RayHitTestParameters rayparams = new RayHitTestParameters(testpoint3D, testdirection);
 
                 //test for a result in the Viewport3D     
-                hitgeo = null;
                 mousePosition = e.GetPosition(win);
                 VisualTreeHelper.HitTest(viewPort, null, HitResult, pointparams);
             }
